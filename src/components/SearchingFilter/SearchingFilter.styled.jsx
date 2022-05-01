8a11025c633e3cd67bce969957acbd2f6f0b3595
vovaukrainets <vovaukrainets@gmail.com>
2022-05-01 16:58:31 +0300

    homework

---
+++ src/components/SearchingFilter/SearchingFilter.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const SearchingWrapper = styled.label`
+    display: flex;
+    flex-direction: column;
+`; 
+export  const SearchingTextArea = styled.input`
+    width: 150px;
+`;
